1e6a08426c1f21d4b11f0bf1529b3bd30aa078ae
Okenkai <71735003+Okenkai@users.noreply.github.com>
2020-10-06 10:53:05 +0200

    ajout navbar et page magasin/restaurant/enseigne

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
+/*********** Login **********/
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
  });
+
+/********** Navbar **********/
+ function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
